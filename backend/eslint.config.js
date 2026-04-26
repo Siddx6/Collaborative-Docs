6a983517694d489ba0da2f0c25d350dcda14fcf8
Siddharth Kumar <siddharthkumar2344@gmail.com>
2026-04-27 04:49:29 +0530

    ci: fix eslint rule causing backend failure

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -16,6 +16,7 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/ban-ts-comment": "warn",
       "@typescript-eslint/no-require-imports": "warn",
+      "@typescript-eslint/only-throw-error": "off",
       "no-console": "warn",
     },
   }
